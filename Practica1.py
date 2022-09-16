3f2d445129b4b37cae2b78e7f910fe2930a35264
ps21-1727 <pamelasanchez21.1727@gmail.com>
2022-09-16 15:31:37 -0400

    Quiero terminar yaaaa

---
+++ Practica1.py
@@ -0,0 +1,21 @@
+#Primero se declaran las variables a utilizar
+num1 = int(input(" Ingrese un digito: "))
+num2 =  int(input("Ingrese otro digito: "))
+
+#Segundo seleccionamos la operacion que queremos realizar
+operacion = input("¿Que operacion desea relaizar suma, division, resta, multiplicacion? ")
+
+#Por ultimo le pones la variable que operacion quiere al seleccionar
+if operacion == "suma": 
+    print("Resultado de la suma: ",num1+num2)
+
+
+elif operacion == "division": 
+    print("Resultado de la division: ",num1/num2)
+    
+
+elif operacion == "resta":
+        print("Resultado de la resta: ",num1-num2)
+
+elif operacion == "multiplicacion":
+     print("Resultado de la division: ",num1*num2)
---
+++ TAR02- Pamela Sanchez.py
@@ -0,0 +1,33 @@
+# Initial commit 
+# ps21-1727 Pamela Sanchez
+# Escribir un programa que pida 2 números y muestre paso a paso el cálculo de la raíz cuadrada 
+# de la suma del cuadrado de ambos.
+
+import math
+
+# Se declara una variable y luego se intruce el digito que desea
+numero1 = int(input("Introduzca un digito: "))
+numero2 = int(input("Introduzca un digito: "))
+
+
+# Se hacen las declaraciones de las operacion matematicas 
+potencia1 = numero1**2
+potencia2 = numero2**2
+suma_de_potencia = potencia1 + potencia2
+
+# Ya aqui vemos el resultado de la operacion
+print ("Numero 1 al cuadro es ", potencia1 )
+print ("Numero 2 al cuadro es ", potencia2 )
+print (" El resultado de la suma es ", suma_de_potencia )
+print ("Raiz cuadrada: ", math.sqrt(suma_de_potencia))
+
+
+
+
+
+
+
+
+
+
+ 
